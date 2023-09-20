875de48ae8c3802d18062bc4dbcdc4b82284a5b1
thowfiq hussain.p <145031657+thowfiq-hussain@users.noreply.github.com>
2023-09-20 18:41:05 +0530

    Update list using total marks.py

--- list using total marks.py
+++ list using total marks.py
@@ -1,13 +1,16 @@
 num_subjects = int(input("Enter the number of subjects: "))
 marks = [ ]
+failed_subject=[ ]
 for i in range(num_subjects):
     subject_mark = float(input(f"Enter the marks for subject {i+1}: "))
     marks.append(subject_mark)
+     if subject_mark < 50:
+         failed_subject.append(i+1)
 total_marks = sum(marks)
 average_marks = total_marks / num_subjects
-
 print("Total marks:", total_marks)
 print("Average marks:", average_marks)
+print("failed subject:",failed_subject)
 if average_marks > 70:
     print("Grade: First Class")
 elif average_marks > 60:
