7e68b1176e8c299a92fcaa94eafa5b516f906875
thowfiq hussain.p <145031657+thowfiq-hussain@users.noreply.github.com>
2023-09-24 10:52:21 +0530

    Update dictionary  by getting marks.py

--- dictionary  by getting marks.py
+++ dictionary  by getting marks.py
@@ -4,12 +4,12 @@ for i in range(subjects):
     subject = input("Enter the subject name: ")
     mark = int(input(f"Enter the marks for {subject}: "))
     marks[subject] = mark
-total_marks = sum(marks.values())
+total_marks = sum(marks.values()) #.values()method returns view object...the view obj contains the values of dictionary ,as like list...
 average_marks = total_marks / subjects
 print("Total Marks:", total_marks)
 print("Average Marks:", average_marks)
 failed_subjects = []
-for subject, mark in marks.items():
+for subject, mark in marks.items():  #.items() method is used to return the list with all dictionary keys with values...
     if mark < 50:
         failed_subjects.append(subject)
         if len(failed_subjects) > 0:
