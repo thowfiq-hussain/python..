9504cdf5191d160b4c20784e0b0349828372ed8e
thowfiq hussain.p <145031657+thowfiq-hussain@users.noreply.github.com>
2023-09-24 10:13:01 +0530

    Update dictionary  by getting marks.py

--- dictionary  by getting marks.py
+++ dictionary  by getting marks.py
@@ -1,26 +1,20 @@
 subjects = int(input("Enter the number of subjects: "))
 marks = {}
-
 for i in range(subjects):
     subject = input("Enter the subject name: ")
     mark = int(input(f"Enter the marks for {subject}: "))
     marks[subject] = mark
-
 total_marks = sum(marks.values())
 average_marks = total_marks / subjects
-
 print("Total Marks:", total_marks)
 print("Average Marks:", average_marks)
-
 failed_subjects = []
 for subject, mark in marks.items():
     if mark < 50:
         failed_subjects.append(subject)
-
-if len(failed_subjects) > 0:
+        if len(failed_subjects) > 0:
     print("Failed Subjects:")
     for subject in failed_subjects:
-        print(subject)
-else:
+        print(subject)else:
     print("CongratulationsYou passed all subjects!")
 
